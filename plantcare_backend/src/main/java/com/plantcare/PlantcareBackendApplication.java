689dfd839c5d56a694a4b7af60c24ba699a8a37d
Ed <edelinsantos.aluno@unipampa.edu.br>
2026-06-20 21:33:02 -0300

    feat(backend): scaffold Spring Boot project with package structure

---
+++ plantcare_backend/src/main/java/com/plantcare/PlantcareBackendApplication.java
@@ -0,0 +1,13 @@
+package com.plantcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantcareBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantcareBackendApplication.class, args);
+	}
+
+}
